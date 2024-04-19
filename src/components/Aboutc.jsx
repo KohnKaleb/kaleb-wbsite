db52f3381943c2aeb25c729e0b27dd5204e726ee
kalebkohn <kjkohn@wisc.edu>
2024-04-18 19:19:43 -0500

    additons to about page and css styling all around

--- src/components/Aboutc.jsx
+++ src/components/Aboutc.jsx
@@ -1,17 +1,52 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { Canvas } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const Section = styled.div`
-    height: 100vh;
-    scroll-snap-align: center;
-`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
+
+const Top = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Logos = styled.div``;
 
 const About = () => {
-    return (
-        <Section id="about">
-            <div>About</div>
-        </Section>
-    )
-}
+  const gltf = useRef();
 
-export default About
+  return (
+    <Section id="about">
+      <div>About</div>
+      <Top>
+        <h1>Kaleb Kohn</h1>
+        <p>
+          I've been coding for over four years dabbling in mobile development,
+          full stack development, AI and data science. I'm a Senior at
+          UW-Madison Graduating in May 2025.
+        </p>
+      </Top>
+      <Bottom>
+        <div>Languages: Python, Java, JavaScript, C, HTML, CSS, SQL</div>
+        <div>
+          Frameworks: React, Vue.js, Spring MVC, Django, Node.js, Express,
+          Android Studio
+        </div>
+        <div ref={gltf}></div>
+        <Logos></Logos>
+      </Bottom>
+    </Section>
+  );
+};
+
+export default About;
--- src/components/Contactc.jsx
+++ src/components/Contactc.jsx
@@ -7,11 +7,46 @@ const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
 `;
-const Container = styled.div``;
+const Container = styled.div`
+`;
 
-const Top = styled.div``;
+const Top = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+  top: 50px;
+`;
 
-const Bottom = styled.div``;
+const Bottom = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+
+  form {
+    width: 20%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 15px;
+  }
+
+  input {
+    width: 100%;
+  }
+
+  #message {
+    height: 100px;
+  }
+`;
+
+const Icons = styled.div`
+  display: flex;
+  gap: 10px;
+`;
 
 const Contact = () => {
   const form = useRef();
@@ -21,9 +56,14 @@ const Contact = () => {
 
     console.log(process.env.REACT_APP_EMAIL_JS_SECRET_KEY);
     emailjs
-      .sendForm(process.env.REACT_APP_EMAIL_JS_SECRET_KEY, process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID, form.current, {
-        publicKey: process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY,
-      })
+      .sendForm(
+        process.env.REACT_APP_EMAIL_JS_SECRET_KEY,
+        process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID,
+        form.current,
+        {
+          publicKey: process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY,
+        }
+      )
       .then(
         () => {
           console.log("SUCCESS!");
@@ -38,31 +78,35 @@ const Contact = () => {
     <Section id="contact">
       <Container>
         <Top>
-          <div>Contact</div>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/KohnKaleb"
-          >
-            <FaGithub />
-          </a>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/kalebkohn/"
-          >
-            <FaLinkedin />
-          </a>
+          <div>Follow me on socials</div>
+          <Icons>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/KohnKaleb"
+            >
+              <FaGithub style={{ fontSize: '4em' }}/>
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/kalebkohn/"
+            >
+              <FaLinkedin style={{ fontSize: '4em' }}/>
+            </a>
+          </Icons>
         </Top>
         <Bottom>
           <div>Reach out to me</div>
           <form ref={form} onSubmit={sendEmail}>
-            <label htmlFor="name">Name:</label>
-            <input id="name" type="text" name="from_name"/>
-            <label htmlFor="email">Email:</label>
-            <input id="email" type="text" name="email"/>
-            <label htmlFor="message">Message</label>
-            <input id="message" type="text" name="message" />
+            <input id="name" type="text" name="from_name" placeholder="name" />
+            <input id="email" type="text" name="email" placeholder="email" />
+            <input
+              id="message"
+              type="text"
+              name="message"
+              placeholder="message"
+            />
             <button type="submit">Submit</button>
           </form>
         </Bottom>
